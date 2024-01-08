4ddfb405648464778b4754142da22b228d412f48
Chirag1761 <chiragparchani1234@gmail.com>
2024-01-08 23:58:31 +0530

    Tailwind and Routing Setup

--- SITCOM/src/App.tsx
+++ SITCOM/src/App.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import './globals.css'
+import SignInform from './auth/forms/SignInform';
+import SignUpform from './auth/forms/SignUpform';
+import Home from './root/pages/Home';
+import AuthLayout from './auth/AuthLayout'
+
+import { Routes, Route } from 'react-router-dom';
+import RootLayout from './root/pages/RootLayout';
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className="flex h-screen">
+        <Routes>
+          {/* {public routes} */}
+          <Route element={<AuthLayout/>}>
+            <Route path="/sign-in" element={<SignInform />}/>
+            <Route path="/sign-up" element={<SignUpform />}/>
+          </Route>
+
+           {/* {private routes} */}
+           <Route element={<RootLayout/>}>
+            <Route index element={<Home />}/>
+           </Route>
+
+        </Routes>
+
+    </main>
   )
 }
 
